7dd700b4d587d7fa6cd0ea03ddb1fcf5b2c0a381
matan3857 <matan38355@gmail.com>
2022-02-10 10:39:27 +0200

    hooks update

--- src/cmps/Hamburger.jsx
+++ src/cmps/Hamburger.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Drawer from "@material-ui/core/Drawer";
@@ -6,76 +6,57 @@ import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import { NavLink } from 'react-router-dom'
 
+export function Hamburger(props) {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
+    return (
+        <AppBar className='app-bar' variant="elevation" position="static" color='default'>
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={() => setIsDrawerOpen(true)}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">MisterToy</Typography>
 
-export class Hamburger extends React.Component {
-
-    state = {
-        isDrawerOpen: false
-    }
-    setIsDrawerOpen = (isDrawerOpen) => {
-        this.setState({
-            isDrawerOpen
-        });
-    }
-
-    toggleDrawer = () => {
-        this.setIsDrawerOpen(!this.state.isDrawerOpen);
-    }
-
-    render() {
-
-        return (
-            <AppBar className='app-bar' variant="elevation" position="static" color='default'>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={() => this.setIsDrawerOpen(true)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6">MisterToy</Typography>
-
-                    <Drawer className='drawer-container' open={this.state.isDrawerOpen} onClose={() => this.setIsDrawerOpen(false)}>
-                        <List className='list-container' style={{ width: '250px' }}>
-                            <ListItem>
-                                <NavLink 
+                <Drawer className='drawer-container' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+                    <List className='list-container' style={{ width: '250px' }}>
+                        <ListItem>
+                            <NavLink
                                 className={({ isActive }) => (isActive ? 'burger-active' : 'burger-inactive' + ' nav-link')}
-                                onClick={this.toggleDrawer} to='/'>Home</NavLink>
-                            </ListItem>
+                                onClick={() => setIsDrawerOpen(!isDrawerOpen)} to='/'>Home</NavLink>
+                        </ListItem>
 
-                            <ListItem>
-                                <NavLink 
+                        <ListItem>
+                            <NavLink
                                 className={({ isActive }) => (isActive ? 'burger-active' : 'burger-inactive' + ' nav-link')}
-                                onClick={this.toggleDrawer} to='/toy'>Toys</NavLink>
-                            </ListItem>
+                                onClick={() => setIsDrawerOpen(!isDrawerOpen)} to='/toy'>Toys</NavLink>
+                        </ListItem>
 
-                            <ListItem>
-                                <NavLink 
+                        <ListItem>
+                            <NavLink
                                 className={({ isActive }) => (isActive ? 'burger-active' : 'burger-inactive' + ' nav-link')}
-                                onClick={this.toggleDrawer} to='/toy/dashboard'>Dashboard</NavLink>
-                            </ListItem>
-                            <ListItem>
-                                <NavLink
-                                    className={({ isActive }) => (isActive ? 'burger-active' : 'burger-inactive' + ' nav-link')}
-                                    onClick={this.toggleDrawer} to='/about'>About</NavLink>
-                            </ListItem>
-                            <ListItem>
-                                <NavLink
-                                    className={({ isActive }) => (isActive ? 'active' : 'inactive' + ' nav-link')}
-                                    onClick={this.toggleDrawer} to='/login'>Login</NavLink>
-                            </ListItem>
-                        </List>
-                    </Drawer>
-                </Toolbar>
-            </AppBar>
-        );
-    }
+                                onClick={() => setIsDrawerOpen(!isDrawerOpen)} to='/toy/dashboard'>Dashboard</NavLink>
+                        </ListItem>
+                        <ListItem>
+                            <NavLink
+                                className={({ isActive }) => (isActive ? 'burger-active' : 'burger-inactive' + ' nav-link')}
+                                onClick={() => setIsDrawerOpen(!isDrawerOpen)} to='/about'>About</NavLink>
+                        </ListItem>
+                        <ListItem>
+                            <NavLink
+                                className={({ isActive }) => (isActive ? 'active' : 'inactive' + ' nav-link')}
+                                onClick={() => setIsDrawerOpen(!isDrawerOpen)} to='/login'>Login</NavLink>
+                        </ListItem>
+                    </List>
+                </Drawer>
+            </Toolbar>
+        </AppBar>
+    );
 }
