51e10f929fe1ded768912fd05eb8c7740920feb8
matan3857 <matan38355@gmail.com>
2022-02-17 17:46:24 +0200

    footer css update

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -4,10 +4,9 @@ export class AppFooter extends React.Component {
     render() {
         return (
             <footer>
-                <a href="/" className="app-name pointer">Mister Toy</a>
                 <div className="contact-us">
                     <a target="_blank" href="https://www.linkedin.com/in/matan-levi-561115199/" className="fab fa-linkedin"></a>
-                    <a target="_blank" href="https://www.linkedin.com/in/matan-levi-561115199/" className="name">Matan</a>
+                    <a target="_blank" href="https://www.linkedin.com/in/matan-levi-561115199/" className="name">Created By Matan Levi</a>
                 </div>
             </footer>
         )
