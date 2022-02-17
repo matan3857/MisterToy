25fea8dcd656a4a18cd4e3653a5988747910d119
matan3857 <matan38355@gmail.com>
2022-02-17 16:17:42 +0200

    styled-form & toy-edit css

--- src/pages/ToyEdit.jsx
+++ src/pages/ToyEdit.jsx
@@ -11,37 +11,39 @@ function _ToyEdit(props) {
     const fetchData = async () => {
       const toy = await toyService.getById(toyId)
       setToy(toy)
-   }
-   fetchData()
+    }
+    fetchData()
   }, []);
 
   const onSubmit = async (ev) => {
     ev.preventDefault();
     await props.editToy(toy)
     props.history.push('/toy')
-};
+  };
 
   if (!toy) return <div> Loading...</div>
   return (
-    <div>
-      <h1>ToyEdit</h1>
-      <form className="edit-toy" onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={toy.name}
-          onChange={(ev) => setToy({ ...toy, name: ev.target.value })}
-          required
-        />
-        <input
-          type="number"
-          name="price"
-          value={toy.price}
-          onChange={(ev) => setToy({ ...toy, price: ev.target.value })}
-          required
-        />
-        <button>Submit Edit</button>
-      </form>
+    <div className='toy-edit'>
+      <div className='styled-container'>
+        <p>Toy Edit</p>
+        <form className="edit-toy" onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={toy.name}
+            onChange={(ev) => setToy({ ...toy, name: ev.target.value })}
+            required
+          />
+          <input
+            type="number"
+            name="price"
+            value={toy.price}
+            onChange={(ev) => setToy({ ...toy, price: ev.target.value })}
+            required
+          />
+          <button className='toy-edit-btn'>Submit Edit</button>
+        </form>
+      </div>
     </div>
   )
 }
